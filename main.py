cfec0f6ca92b7957d2675c272ac5e708180f04d2
장준혁 <jangjunhyeok@jangjunhyeog-ui-MacBookAir.local>
2023-05-09 15:19:36 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+from flask import Flask
+import requests
+import time
+from plyer import notification
+app = Flask(__name__)
+
+@app.route('/')
+def route_main():
+    res = requests.get('https://sports-api.named.com/v1.0/sports/soccer/games/11229398/broadcasts')
+    print(res.text)
+    
+    notification.notify(
+        title = 'test test',
+        message = '메시지 내용',
+        app_name = '테스트앱',
+        timeout = 10,
+    )
+    return '<h1>Hello World!</h1>'
+
+if __name__ == "__main__":
+    app.run(port="8081")
